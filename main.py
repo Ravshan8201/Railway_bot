516909c87bc74db13ffdf7876d60cbd12a678d16
Ravshan8201 <88588235+Ravshan8201@users.noreply.github.com>
2021-12-10 20:01:55 +0500

    Update main.py

--- main.py
+++ main.py
@@ -46,6 +46,35 @@ dis.add_handler(CallbackQueryHandler(pattern='ru', callback=ru))
 dis.add_handler(CallbackQueryHandler(pattern='uz', callback=uz))
 dis.add_handler(MessageHandler(Filters.text, next_func))
 
+import logging
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+
+# Define a few command handlers. These usually take the two arguments bot and
+# update. Error handlers also receive the raised TelegramError object in error.
+def start(bot, update):
+    """Send a message when the command /start is issued."""
+    bot.send_message(chat_id=update.message.chat_id, text='Hi!')
+
+
+def help(bot, update):
+    """Send a message when the command /help is issued."""
+    bot.send_message(chat_id=update.message.chat_id, text='Help!')
+
+
+def echo(bot, update):
+    """Echo the user message."""
+    bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
+
+
+def error(bot, update):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', bot, update.error)
+
 def main():
     if WEBHOOK:
         LOGGER.info("Using webhooks.")
