ccdd35ea7c25725c529d806df2907d25203daf92
Ravshan8201 <88588235+Ravshan8201@users.noreply.github.com>
2021-12-10 18:27:52 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 from telegram.ext import Updater, Dispatcher, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
 from func import *
-from tg_bot import dispatcher, updater, TOKEN, WEBHOOK, OWNER_ID, DONATION_LINK, CERT_PATH, PORT, URL, LOGGER, \
-    ALLOW_EXCL
-
-
 
 from cons import *
 upd = Updater(token=TOKEN, workers=4)
