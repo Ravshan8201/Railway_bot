d0042dd1c30f7a988eb80147ada985eafd8ddb85
Ravshan8201 <88588235+Ravshan8201@users.noreply.github.com>
2021-12-10 17:40:24 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from telegram.ext import Updater, Dispatcher, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
 from func import *
+from tg_bot import dispatcher, updater, TOKEN, WEBHOOK, OWNER_ID, DONATION_LINK, CERT_PATH, PORT, URL, LOGGER, \
+    ALLOW_EXCL
 
 from cons import *
 upd = Updater(token=TOKEN, workers=4)
@@ -8,4 +10,27 @@ dis.add_handler(CommandHandler(command='start', callback=start))
 dis.add_handler(CallbackQueryHandler(pattern='ru', callback=ru))
 dis.add_handler(CallbackQueryHandler(pattern='uz', callback=uz))
 dis.add_handler(MessageHandler(Filters.text, next_func))
-upd.start_polling(drop_pending_updates=True)
+
+def main():
+    if WEBHOOK:
+        LOGGER.info("Using webhooks.")
+        upd.start_webhook(listen="0.0.0.0",
+                              port=PORT,
+                              url_path=TOKEN)
+
+        if CERT_PATH:
+            upd.bot.set_webhook(url=URL + TOKEN,
+                                    certificate=open(CERT_PATH, 'rb'))
+        else:
+            upd.bot.set_webhook(url=URL + TOKEN)
+
+    else:
+        LOGGER.info("Using long polling.")
+        upd.start_polling(timeout=15, read_latency=4)
+
+    upd.idle()
+
+
+if __name__ == 'main':
+    LOGGER.info("Successfully loaded modules: " + str(ALL_MODULES))
+    main()
