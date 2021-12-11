dd6d1ecd02922d650b9bbaa832f503b2503d86fa
Ravshan8201 <88588235+Ravshan8201@users.noreply.github.com>
2021-12-11 16:13:02 +0500

    Create sql_cons_promo.py

---
+++ sql_cons_promo.py
@@ -0,0 +1,34 @@
+import sqlite3
+conn = sqlite3.connect('user_list.sqlite')
+cur = conn.cursor()
+cur.execute('''
+CREATE TABLE IF NOT EXISTS Promo_list(
+Promo STRING,
+Name_uz STRING,
+Name_ru STRING
+)
+''')
+first_insetd = '''
+INSERT INTO Promo_list VALUES ('{}',' ',' ')
+'''
+
+promo_id = '''
+SELECT Promo 
+FROM Promo_list
+Where Promo = '{}'
+'''
+promo_upd = '''
+UPDATE Promo_list  
+SET Promo = '{}'
+Where Promo = '{}'
+'''
+name_uz_id = '''
+SELECT Name_uz 
+FROM Promo_list
+Where Promo = '{}'
+'''
+name_uz_upd = '''
+UPDATE Promo_list  
+SET Name_uz = '{}'
+Where Promo = '{}'
+'''
