531230eb65c6c4a958d58ad2ff1c44e2e2fdc20c
Ravshan8201 <88588235+Ravshan8201@users.noreply.github.com>
2021-12-18 18:53:13 +0500

    Update main2.py

--- main2.py
+++ main2.py
@@ -7,10 +7,17 @@ dis.add_handler(CommandHandler(command='start', callback=start))
 dis.add_handler(CallbackQueryHandler(pattern='admin', callback=admin))
 dis.add_handler(CallbackQueryHandler(pattern='aksiya_tamom', callback=aksiya_tamom))
 dis.add_handler(CallbackQueryHandler(pattern='sov', callback=sov))
+dis.add_handler(CallbackQueryHandler(pattern='pro_num', callback=pro_num))
 dis.add_handler(CallbackQueryHandler(pattern='ru', callback=ru))
 dis.add_handler(CallbackQueryHandler(pattern='uz', callback=uz))
 dis.add_handler(MessageHandler(Filters.text, next_func))
 dis.add_handler(MessageHandler(Filters.contact, get_contac))
 dis.add_handler(MessageHandler(Filters.photo, adm))
 dis.add_handler(MessageHandler(Filters.video, adm_v))
+def error(bot, update, error):
+    if not (error.message == "Message is not modified"):
+        logger.warning('Update "%s" caused error "%s"' % (update, error))
+
+    upd.dispatcher.logger.addFilter(
+        (lambda s: not s.msg.endswith('A TelegramError was raised while processing the Update')))
 upd.start_polling(drop_pending_updates=True)
