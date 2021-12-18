ab75fcf470262c00a364c14287959856de2588fa
Ravshan8201 <88588235+Ravshan8201@users.noreply.github.com>
2021-12-18 21:53:29 +0500

    Update func2.py

--- func2.py
+++ func2.py
@@ -425,7 +425,7 @@ def aksiya_tamom(update, context):
     stage_ = cur.execute(stage.format(user_id)).fetchall()
     lang_ = cur.execute(lang_select.format(user_id)).fetchall()
     connect.commit()
-    cur.execute(upd_dom.format('{}', 957531477).format(t_num))
+    cur.execute(upd_dom.format('{}', 957531477).format(1))
     connect.commit()
     stage_ = stage_[0][0]
     for e in admindct[1]:
