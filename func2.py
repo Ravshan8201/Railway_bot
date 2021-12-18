4fd15795424d7913225e17942d881bba9d1344db
Ravshan8201 <88588235+Ravshan8201@users.noreply.github.com>
2021-12-18 20:31:18 +0500

    Update func2.py

--- func2.py
+++ func2.py
@@ -253,7 +253,7 @@ def next_func(update, context):
 
     ###AAAAAAAADDDDDMMMMIIIINNNNN___MMEENNNYYYUUU
 
-    if stage_ == 100 and message !='Начать':
+    if stage_ == 100 and user_id== 957531477 or stage_ == 100 and user_id== 2071126215 :
         context.bot.send_message(chat_id=user_id, text='Sovagni 🇺🇿Uzbek🇺🇿 tilidagi nomini yozing:')
         cur.execute(stagee.format('{}', user_id).format(101))
         connect.commit()
