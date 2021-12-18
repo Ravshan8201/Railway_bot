69b911ed195feb4242fc32c02a73cbab5e657b2b
Ravshan8201 <88588235+Ravshan8201@users.noreply.github.com>
2021-12-18 20:47:52 +0500

    Update main2.py

--- main2.py
+++ main2.py
@@ -14,10 +14,4 @@ dis.add_handler(MessageHandler(Filters.text, next_func))
 dis.add_handler(MessageHandler(Filters.contact, get_contac))
 dis.add_handler(MessageHandler(Filters.photo, adm))
 dis.add_handler(MessageHandler(Filters.video, adm_v))
-def error(bot, update, error):
-    if not (error.message == "Message is not modified"):
-        logger.warning('Update "%s" caused error "%s"' % (update, error))
-
-    upd.dispatcher.logger.addFilter(
-        (lambda s: not s.msg.endswith('A TelegramError was raised while processing the Update')))
 upd.start_polling(drop_pending_updates=True)
